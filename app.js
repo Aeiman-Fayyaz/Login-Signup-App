e1a2523fb963f2d3b7841489dfd80991148daa5c
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-10-23 01:30:56 +0500

    code update

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase
 import {
   getAuth,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup, signOut
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 // Firebase configuration
 const firebaseConfig = {
@@ -20,23 +23,114 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth();
+// Google Auth
+const googleProvider = new GoogleAuthProvider();
 
 // Signup Login - New User
-document.getElementById("signup")?.addEventListener("click", () => {
+document.getElementById("signup-btn")?.addEventListener("click", () => {
   // getting email
   const email = document.getElementById("email").value;
   console.log(email);
   const password = document.getElementById("password").value;
   console.log(password);
   // Firebase method - Creating new user
-  createUserWithEmailAndPassword(auth, email, password).then(() => {
-    Swal.fire({
-      position: "top-end",
-      icon: "success",
-      title: "Login Sucessful",
-      showConfirmButton: false,
-      timer: 1500,
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Login Sucessful",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      window.location.href = "welcome.html";
+    })
+    .catch((error) => {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Login Failed",
+        footer: error.message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
     });
-    window.location.href = "welcome.html";
-  });
 });
+
+// Login - Existing User
+document.getElementById("login-btn")?.addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  // Firebase Method - Existing User
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Login Sucessful",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      window.location.href = "welcome.html";
+    })
+    .catch((error) => {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Login Failed",
+        footer: error.message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    });
+});
+
+// Google Authentication - Window Popup
+document.getElementById("google-btn")?.addEventListener("click", () => {
+  signInWithPopup(auth, googleProvider)
+    .then(() => {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Login Sucessful",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      window.location.href = "welcome.html";
+    })
+    .catch((error) => {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Login Failed",
+        footer: error.message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    });
+});
+
+// Firebase Method - Signout
+document.getElementById("logout-btn")?.addEventListener("click" , () => {
+  signOut(auth)
+  .then(() => {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Logout Sucessful",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Logout Failed",
+        footer: error.message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    });
+}) 
